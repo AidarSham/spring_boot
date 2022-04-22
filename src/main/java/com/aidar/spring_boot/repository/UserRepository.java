3d162813a7cfe565420e5d1c3da3785f3ebd52bc
AidarSham <aidar.sham@mail.ru>
2022-04-22 17:36:56 +0500

    Перенос приложения на Spring boot

---
+++ src/main/java/com/aidar/spring_boot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.aidar.spring_boot.repository;
+
+import com.aidar.spring_boot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
